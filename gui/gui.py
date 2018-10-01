dfeba53f5ef8a14b450ad4bef74e3669e74d64f5
Olivia Lyons <32911243+ojlyons@users.noreply.github.com>
2018-10-01 15:54:27 -0400

    started making save button work right

--- gui/gui.py
+++ gui/gui.py
@@ -1,7 +1,7 @@
-
 import tkinter as tk
 from tkinter import filedialog
 from tkinter.ttk import Separator
+
 
 # Used by upload buttons to open a file browser
 def callback():
@@ -10,13 +10,20 @@ def callback():
     print(name2.read())
 
 
+# This is going to open a file browser
+# and the user will specify where they want the output to be saved to.
+def save_output():
+    name = filedialog.asksaveasfile(mode='w', title='Save output',
+                                    filetypes=(("CSV files", "*.csv"), ("All files", "*.*")))
+    print(name.read())
+
+
 #################################################################
 ## SOME VARIABLES
 ## We are defining some stuff that will be used later
 ##
 
 smcm_blue = '#1d285a'
-#logo_path = 'C:\\Users\\oj8ly\\AppData\\Local\\Programs\\Python\\Python37-32\\FinalsScheduleProject\\college-logo.gif'
 logo_path = "images\\college-logo.gif"
 print_icon_path = 'images\\print-icon.gif'
 
@@ -28,7 +35,7 @@ print_icon_path = 'images\\print-icon.gif'
 # Window formatting
 root = tk.Tk()  # The window object
 root.geometry("300x400")
-root.resizable(False,False)
+root.resizable(False, False)
 root.title("SMCM Exam Scheduler")
 root['padx'] = 30
 root['pady'] = 30
@@ -57,7 +64,7 @@ title.config(font=('calibri', 14), foreground=smcm_blue, bg='white')
 text_1_str = "Upload course schedule .csv file:"
 text_1 = tk.Message(root, text=text_1_str, width=1000, bg='white')
 
-text_2_str="Upload finals schedule .csv file:"
+text_2_str = "Upload finals schedule .csv file:"
 text_2 = tk.Message(root, text=text_2_str, width=1000, bg='white')
 
 # Upload buttons
@@ -66,7 +73,7 @@ upload_cschedule_button = tk.Button(root, text='Browse...', command=callback)
 upload_fschedule_button = tk.Button(root, text='Browse...', command=callback)
 
 # Output buttons
-save_output_button = tk.Button(root, text='Save Output...', command=callback)
+save_output_button = tk.Button(root, text='Save Output...', command=save_output)
 
 display_output_button = tk.Button(root, text='Display Output...', command=callback)
 
@@ -76,16 +83,16 @@ print_button = tk.Button(root, image=print_icon, width=25, height=25, command=ca
 # Put all the widgets into the window with a whole bunch of formatting
 #
 
-logo_widget.grid(               row=0, column=0, columnspan=2)
-title.grid(                     row=1, column=0, columnspan=2)
-sep.grid(                       row=2, column=0, columnspan=2,                       sticky='ew')
-text_1.grid(                    row=3, column=0, columnspan=2, padx=0,  pady=(20,5), sticky="W")
-upload_cschedule_button.grid(   row=4, column=0, columnspan=2, padx=20)
-text_2.grid(                    row=5, column=0, columnspan=2, padx=0,  pady=(10,5), sticky='W')
-upload_fschedule_button.grid(   row=6, column=0, columnspan=2, padx=20)
-save_output_button.grid(        row=7, column=0,               padx=12, pady=(20,0), sticky='E')
-display_output_button.grid(     row=7, column=1,                        pady=(20,0), sticky='W')
-print_button.grid(              row=8, column=0, columnspan=2,          pady=(10,0))
+logo_widget.grid(row=0, column=0, columnspan=2)
+title.grid(row=1, column=0, columnspan=2)
+sep.grid(row=2, column=0, columnspan=2, sticky='ew')
+text_1.grid(row=3, column=0, columnspan=2, padx=0, pady=(20, 5), sticky="W")
+upload_cschedule_button.grid(row=4, column=0, columnspan=2, padx=20)
+text_2.grid(row=5, column=0, columnspan=2, padx=0, pady=(10, 5), sticky='W')
+upload_fschedule_button.grid(row=6, column=0, columnspan=2, padx=20)
+save_output_button.grid(row=7, column=0, padx=12, pady=(20, 0), sticky='E')
+display_output_button.grid(row=7, column=1, pady=(20, 0), sticky='W')
+print_button.grid(row=8, column=0, columnspan=2, pady=(10, 0))
 
 # Make the window persistent
 root.mainloop()
