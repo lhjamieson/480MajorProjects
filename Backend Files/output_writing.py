425c8de9645cc422a06647acca23a04349510058
jdkaufman1 <jdkaufman1@smcm.edu>
2018-10-10 17:00:19 -0400

    Massive overhaul of the datastructures used

--- Backend Files/output_writing.py
+++ Backend Files/output_writing.py
@@ -1,6 +1,5 @@
 ##James Kaufman
-##import out packages
-
+##import our packages
 import csv
 import os
 import os.path
@@ -8,34 +7,14 @@ import io
 import pandas as pd
 from pandas import DataFrame, read_csv
 
-#we currently have a 2d list(array)
-test = [["Sam", "Tim", "Jon"], ["Elizabeth", "Magnus", "Kazuma"], ["Axelle", "Magdalena", "Geoff"]]
+##Create two mock columns for the spreadsheet
 names = ["Sam", "Tim", "Jon", "Elizabeth", "Magnus", "Kazuma", "Axelle", "Magdalena", "Geoff"]
 ages = ["21", "45", "32", "13", "16", "20", "24", "25", "67"]
-
+##Create our list
 DataSet = list(zip(names, ages))
-print(DataSet)
-
-df = pd.DataFrame(data = DataSet, columns = ['Names', 'Ages'])
-print(df)
-
-df.to_csv('hi_there.csv',index=False,header=["one", "two"])
-
-
-#let's write it to a csv file!
-# dir = r"C:\Users\13014\Desktop"
-#
-#
-# if not os.path.exists(dir):
-#     os.mkdir(dir)
-#
-# with open(os.path.join(dir, "test"+'.csv'), "w") as f:
-#   csvfile=io.StringIO()
-#   csvwriter=csv.writer(f)
-#   for l in test: #l is each array in test
-#       #print("hi")
-#       # for x in l:#x is each individual element
-#       #     print(x)
-#       csvwriter.writerow(l)
-#   # for a in csvfile.getvalue():
-#   #   f.writelines(a)
+##Specify the column names
+dataColumns = ['Names', 'Ages']
+##Create the DataFrame
+df = pd.DataFrame(data = DataSet, columns = dataColumns)
+##Convert the DataFrame to a CSV file in the file directory
+df.to_csv('hi_there.csv',index=False,header=dataColumns)
