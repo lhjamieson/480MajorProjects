b595fba8ddb996dea5ce9814bb7b8e96a77025ad
jdkaufman1 <jdkaufman1@smcm.edu>
2018-10-10 16:54:43 -0400

    adding output_writing

--- Backend Files/output_writing.py
+++ Backend Files/output_writing.py
@@ -1,15 +1,41 @@
 ##James Kaufman
 ##import out packages
+
 import csv
+import os
+import os.path
+import io
+import pandas as pd
+from pandas import DataFrame, read_csv
+
+#we currently have a 2d list(array)
+test = [["Sam", "Tim", "Jon"], ["Elizabeth", "Magnus", "Kazuma"], ["Axelle", "Magdalena", "Geoff"]]
+names = ["Sam", "Tim", "Jon", "Elizabeth", "Magnus", "Kazuma", "Axelle", "Magdalena", "Geoff"]
+ages = ["21", "45", "32", "13", "16", "20", "24", "25", "67"]
+
+DataSet = list(zip(names, ages))
+print(DataSet)
+
+df = pd.DataFrame(data = DataSet, columns = ['Names', 'Ages'])
+print(df)
+
+df.to_csv('hi_there.csv',index=False,header=["one", "two"])
 
 
-
-##we currently have a 2d list(array)
-test = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-##let's write it to a csv file!
-
-with open('test.csv', 'wb', newline = '') as csvfile:
-    filewriter = csv.writer(csvfile, delimiter = ' ', quotechar = "|, quoting = csv.QUOTE.MINIMAL")
-    for x in range(0, len(test)):
-        filewiter.writerow(test[x])
+#let's write it to a csv file!
+# dir = r"C:\Users\13014\Desktop"
+#
+#
+# if not os.path.exists(dir):
+#     os.mkdir(dir)
+#
+# with open(os.path.join(dir, "test"+'.csv'), "w") as f:
+#   csvfile=io.StringIO()
+#   csvwriter=csv.writer(f)
+#   for l in test: #l is each array in test
+#       #print("hi")
+#       # for x in l:#x is each individual element
+#       #     print(x)
+#       csvwriter.writerow(l)
+#   # for a in csvfile.getvalue():
+#   #   f.writelines(a)
