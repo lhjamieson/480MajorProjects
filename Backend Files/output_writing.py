bc29505b4f9a775946ef80afcb71e8f0d8020832
jdkaufman1 <jdkaufman1@smcm.edu>
2018-10-15 15:43:31 -0400

    Taking in Gui from frontend

--- Backend Files/output_writing.py
+++ Backend Files/output_writing.py
@@ -7,14 +7,14 @@ import io
 import pandas as pd
 from pandas import DataFrame, read_csv
 
-##Create two mock columns for the spreadsheet
+    ##Create two mock columns for the spreadsheet
 names = ["Sam", "Tim", "Jon", "Elizabeth", "Magnus", "Kazuma", "Axelle", "Magdalena", "Geoff"]
 ages = ["21", "45", "32", "13", "16", "20", "24", "25", "67"]
-##Create our list
+    ##Create our list
 DataSet = list(zip(names, ages))
-##Specify the column names
+    ##Specify the column names
 dataColumns = ['Names', 'Ages']
-##Create the DataFrame
+    ##Create the DataFrame
 df = pd.DataFrame(data = DataSet, columns = dataColumns)
-##Convert the DataFrame to a CSV file in the file directory
+    ##Convert the DataFrame to a CSV file in the file directory
 df.to_csv('hi_there.csv',index=False,header=dataColumns)
