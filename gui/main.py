abb569fd0f37399e62f243a8aa388ba951bb2975
Ahkhan81 <43010537+Ahkhan81@users.noreply.github.com>
2018-10-24 15:20:05 -0400

    Update main.py

--- gui/main.py
+++ gui/main.py
@@ -1,88 +1,170 @@
-# TODO drop classes without meeting days/room numbers? not sure how to reference empty matrix cell
-# if dirtyCLM[x][15] == '':
-#   print('skipped class with no room number')
-# else:
-#   CourseListMatrix[x][6] = dirtyCLM[x][15]
-# TODO figure out why column titles on the es iteration are overwritten
-# TODO remove courses without finals from input
+# code description: The below code incorperates the front end GUI with back end input functionallity
 
-# import pandas a powerful data science module for python
+# if everything works the two browse buttons should be configured for a user to select an xlsx file,
+# have the contents brought into a python matrix, and then printed to console
+
+# indexing issues might occur - length and width are not fully dynamic with varrying xlsx file format
+# do not forget to configure the image file paths to your local machine
+
+# Things to consider:
+
+# as a user i will want to see the file path and the selected file before i tell the program to continue
+# for the user this ensures their selection was received by the program and confirms the file selected is the one they
+# intend to work with
+
+# i think the display output should show the file contents within the GUI and if the user wishes to save the file
+# and open it in excel they have that option. i think the user should be able to preview content before saving it
+
+# currently the print icon is cut off at the bottom of the gui - maybe allow for resizing of window
+
 import pandas as pd
+import tkinter as tk
+import re
+from tkinter import filedialog
+from tkinter.ttk import Separator
 
-# fill a data frame with the CourseList(cl) and ExamSchedule(es)
-# user will feed program this address
-dfcl = pd.read_excel('C:\\Programming Software\\Pycharm\\480MajorProjects\\Input Test Files\\cl.xlsx')
-dfes = pd.read_excel('C:\\Programming Software\\Pycharm\\480MajorProjects\\Input Test Files\\es.xlsx')
+CSM = [[0][0]]
+ESM = [[0][0]]
+output = [[0][0]]
 
-# print to make sure data is good
-# print(dfcl)
-# print(dfes)
-
-dirtyCLM = dfcl.as_matrix(columns=None)
-
-# in our matrix the important data can be found in the columns as follows
-# Course Number      - Column 0
-# Course Title       - Column 1
-# Course Number      - Column 5
-# Class Meeting Time - Column 9
-# Class Meeting Days - Column 13
-# Building Code      - Column 14
-# Room Number        - Column 15
-# every row is new class
-
-# munge
-w, h = len(dfcl.columns), len(dfcl.index);
-CourseListMatrix = [[0 for x in range(w)] for y in range(h)]
-
-# starting with header row because we know that data
-CourseListMatrix[0] = [['Course Number'], ['Course Title'], ['Section Number'], ['Class Meeting Time'],
-                       ['Class Meeting Days'], ['Building Code'], ['Room Number']]
-
-# now have to iterate through the dirty matrix on proper columns to fill CourseListMatrix
-for x in range(len(dfcl.index)):
-    CourseListMatrix[x][0] = dirtyCLM[x][0]  # Course Number
-    CourseListMatrix[x][1] = dirtyCLM[x][1]  # Course title
-    CourseListMatrix[x][2] = dirtyCLM[x][5]  # Course Number
-    CourseListMatrix[x][3] = dirtyCLM[x][9]  # Class Meeting Time
-    CourseListMatrix[x][4] = dirtyCLM[x][13]  # Class Meeting Days;
-    CourseListMatrix[x][5] = dirtyCLM[x][14]  # Building Code
-    CourseListMatrix[x][6] = dirtyCLM[x][15]  # Room number
-    print(CourseListMatrix[x][0], CourseListMatrix[x][1], CourseListMatrix[x][2], CourseListMatrix[x][3],
-          CourseListMatrix[x][4], CourseListMatrix[x][5], CourseListMatrix[x][6])
-print()
-print()
-print()
-# now for ExamSchedule(es)
-dirtyESM = dfes.as_matrix(columns=None)
-w, h = len(dfes.columns), len(dfes.index);
-
-ExamScheduleMatrix = [[0 for x in range(w)] for y in range(h)]
-ExamScheduleMatrix[0] = [['Course Meeting Time'], ['Course Meeting Days'], ['Exam Date'], ['Exam Begin Time'],
-                         ['Exam End Time']]
-# not sure why the column titles are overwritten on es iterations but not cl iterations ¯\_(ツ)_/¯
-# for now just hard coding a print of the first row before it gets overwritten by for loop
-# print(CourseListMatrix[0])
-for x in range(len(dfes.index)):
-    # also need to get which exam times correspond to course meeting days
-    ExamScheduleMatrix[x][0] = dirtyESM[x][0]  # Course meeting days
-    ExamScheduleMatrix[x][1] = dirtyESM[x][1]  # Course meeting times
-    ExamScheduleMatrix[x][2] = dirtyESM[x][2]  # Exam date
-    ExamScheduleMatrix[x][3] = dirtyESM[x][3]  # Exam start time
-    ExamScheduleMatrix[x][4] = dirtyESM[x][4]  # Exam end date
-    # print(ExamScheduleMatrix[x][0])  # print for debugging
+# For test purposes
+def empty_button():
+    print("You pressed that button.")
 
 
-# TODO: Create function to assign courses to exam times
-# function will take in the CourseScheduleMatrix(CSM) and ExamScheduleMatrix(ESM)
-# function will output a correctly formatted list of the courses and their assigned final exam times & dates
-def exam_assignment(CSM, ESM):
-    # function can be split into 3 parts
-    # parts 1 and 2 will be inside of a for loop:
+# Used by exam schedule upload button to open a file browser
+def upload_callback():
+    name2 = filedialog.askopenfile(mode='rb', initialdir='/', title='Select a file',
+                                   filetypes=(("CSV files", "*.csv"), ("Excel files", "*.xlsx"), ("All files", "*.*")))
+    print(name2)
+    CLexcelToMatrix(name2)
 
-    temp_output = [[0 for i in range(8)] for j in range(len(CSM))]
+# Used by course upload buttons to open a file browser
+def upload_callback2():
+    name2 = filedialog.askopenfile(mode='rb', initialdir='/', title='Select a file',
+                                   filetypes=(("CSV files", "*.csv"), ("Excel files", "*.xlsx"), ("All files", "*.*")))
+    ESexcelToMatrix(name2)
 
+
+# This is going to open a file browser
+# and the user will specify where they want the output to be saved to.
+def save_output():
+    name = filedialog.asksaveasfile(mode='w', title='Save output',
+                                    filetypes=(("CSV files", "*.csv"), ("Excel files", "*.xlsx"), ("All files", "*.*")))
+    exam_assignment()
+    output_writing(name)
+
+
+# Called when info button is pressed
+# Prints the INFO section of the README file
+# Someday, we will make it so this function causes a popup with the info printed onto it
+def info_callback():
+    README_path = 'README'
+    f = open(README_path, 'r')
+    lines = f.readlines()
+    for line in lines:
+        if line.__contains__('HELP'):
+            break
+        print(line)
+    f.close()
+    print('info button was successfully pressed')
+
+
+# Called when help button is pressed
+# Prints the HELP section of the README file
+# Someday, we will make it so this function causes a popup with the info printed onto it
+def help_callback():
+    README_path = 'README'
+    f = open(README_path, 'r')
+    lines = f.readlines()
+    HELP_flag = False
+    for line in lines:
+        if line.__contains__('HELP'):
+            HELP_flag = True
+        if HELP_flag:
+            print(line)
+    f.close()
+    print('help button was successfully pressed')
+
+def ESexcelToMatrix(fa):
+    print('Converting Exam Schedual to Python Matrix')
+    fileAddress = fa
+    dfes = pd.read_excel(treatSelectedAddress(fileAddress))
+    dirtyESM = dfes.as_matrix(columns=None)
+    global ESM
+    ESM = dirtyESM
+
+
+
+def CLexcelToMatrix(fa):
+    print('Converting Course List to Python Matrix')
+    fileAddress = fa
+    dfcl = pd.read_excel(treatSelectedAddress(fileAddress))
+    dirtyCLM = dfcl.as_matrix(columns=None)
+    w, h = 7, len(dfcl.index)
+    CourseListMatrix = [[0 for x in range(w)] for y in range(h)]
+    CourseListMatrix[0] = [['Course Number'], ['Course Title'], ['Section Number'], ['Class Meeting Time'],
+                           ['Class Meeting Days'], ['Building Code'], ['Room Number']]
+    course_location = 1
+    for x in range(len(dfcl.index)):
+        try:
+            ed = dirtyCLM[x][8]
+            end_d = ed.strftime('%Y-%m-%d')
+            end_date = end_d[5:7]
+            if re.match(r'12', end_date):
+                CourseListMatrix[course_location][0] = dirtyCLM[x][0]
+                CourseListMatrix[course_location][1] = dirtyCLM[x][1]
+                CourseListMatrix[course_location][2] = dirtyCLM[x][5]
+                CourseListMatrix[course_location][3] = dirtyCLM[x][9]
+                CourseListMatrix[course_location][4] = dirtyCLM[x][13]
+                CourseListMatrix[course_location][5] = dirtyCLM[x][14]
+                CourseListMatrix[course_location][6] = dirtyCLM[x][15]
+                course_location += 1
+            elif re.match(r'11', end_date):
+                CourseListMatrix[course_location][0] = dirtyCLM[x][0]
+                CourseListMatrix[course_location][1] = dirtyCLM[x][1]
+                CourseListMatrix[course_location][2] = dirtyCLM[x][5]
+                CourseListMatrix[course_location][3] = dirtyCLM[x][9]
+                CourseListMatrix[course_location][4] = dirtyCLM[x][13]
+                CourseListMatrix[course_location][5] = dirtyCLM[x][14]
+                CourseListMatrix[course_location][6] = dirtyCLM[x][15]
+                course_location += 1
+            elif re.match(r'04', end_date):
+                CourseListMatrix[course_location][0] = dirtyCLM[x][0]
+                CourseListMatrix[course_location][1] = dirtyCLM[x][1]
+                CourseListMatrix[course_location][2] = dirtyCLM[x][5]
+                CourseListMatrix[course_location][3] = dirtyCLM[x][9]
+                CourseListMatrix[course_location][4] = dirtyCLM[x][13]
+                CourseListMatrix[course_location][5] = dirtyCLM[x][14]
+                CourseListMatrix[course_location][6] = dirtyCLM[x][15]
+                course_location += 1
+            elif re.match(r'05', end_date):
+                CourseListMatrix[course_location][0] = dirtyCLM[x][0]
+                CourseListMatrix[course_location][1] = dirtyCLM[x][1]
+                CourseListMatrix[course_location][2] = dirtyCLM[x][5]
+                CourseListMatrix[course_location][3] = dirtyCLM[x][9]
+                CourseListMatrix[course_location][4] = dirtyCLM[x][13]
+                CourseListMatrix[course_location][5] = dirtyCLM[x][14]
+                CourseListMatrix[course_location][6] = dirtyCLM[x][15]
+                course_location += 1
+        except AttributeError:
+            cl = 0
+    global CSM
+    CSM = CourseListMatrix
+
+
+def treatSelectedAddress(fileAddress):
+    strFileAddress = fileAddress.name
+    strFileAddress = strFileAddress.replace("/", "\\\\")
+    return strFileAddress
+
+def exam_assignment():
+    global CSM
+    global ESM
+    global output
+    output_location = 0
+    output = [[0 for i in range(8)] for j in range(len(CSM))]
     for x in range(len(CSM)):  # for each course in course list:
-        # PART 1: Harrison
         # get course meeting days and time
         # based on a course's meeting days and time, assign it to the matching exam time and date
         # CSM[x][4] Course meeting days; course meeting days has 7 slots,
@@ -96,27 +178,125 @@ def exam_assignment(CSM, ESM):
         if isinstance(CSM[x][4], str):
             # remove first character in string
             CSM[x][4] = CSM[x][4][1:7]
+        # Check if course has a meeting time
+            for y in range(len(ESM)):  # for each exam time in exam schedule
+                # if course meeting days match exam schedule course meeting days,
+                # and Course start time match exam schedule course start time
+                if CSM[x][4] == ESM[y][0] and CSM[x][3] == ESM[y][1]:
+                    output[output_location][0] = CSM[x][0]  # course number
+                    output[output_location][1] = CSM[x][1]  # course title
+                    output[output_location][2] = CSM[x][2]  # section number
+                    output[output_location][3] = CSM[x][5]  # building code
+                    output[output_location][4] = CSM[x][6]  # room number
+                    output[output_location][5] = ESM[y][2]  # exam date
+                    output[output_location][6] = ESM[y][3]  # exam start time
+                    output[output_location][7] = ESM[y][4]  # exam end time
+                    output_location += 1
 
-        for y in range(len(ESM)):  # for each exam time in exam schedule
-            # if course meeting days match exam schedule course meeting days,
-            # and Course start time match exam schedule course start time
-            if CSM[x][4] == ESM[y][0] and CSM[x][3] == ESM[y][1]:
-                temp_output[x][5] = ESM[y][2]  # exam date
-                temp_output[x][6] = ESM[y][3]  # exam start time
-                temp_output[x][7] = ESM[y][4]  # exam end time
-
-    # PART 2: Sinclair
-    # copy over Course Number, Course Title, Section number
-    # assign exam rooms and buildings based on course meeting room and building
-    # handle room reassignment if necessary
-
-    # PART 3:
-    # once for loop is finished
-    # check formatting
-    # output final list with courses and final exam assignments
-    # output needs: Course Number, Course Title, Section Number, Building code, Room Number, Exam Date, Exam Start Time, Exam End Time
-    return temp_output
+    # output: Course Number, Course Title, Section Number, Building code, Room Number, Exam Date, Exam Start Time, Exam End Time
 
 
-print(exam_assignment(CourseListMatrix, ExamScheduleMatrix))
+def output_writing(name):
+    global output
+    dataColumns = ['Course Number', 'Course Title', 'Section Number', 'Building Code', 'Room Number', 'Exam Date', 'Exam Start Time', 'Exam End Time']
+    df = pd.DataFrame(data = output, columns = dataColumns)
+    df.to_csv(name, index=False, header=dataColumns)
 
+
+def GUI():
+    #################################################################
+    ## SOME VARIABLES
+    ## We are defining some stuff that will be used later
+    ##
+
+    smcm_blue = '#1d285a'
+    logo_path = "images\\college-logo.gif"
+    print_icon_path = 'images\\print-icon.gif'
+    help_icon_path = 'images\\help-icon.gif'
+    about_icon_path = 'images\\info-icon.gif'
+    seahawk_icon_path = 'images\\seahawk-icon.ico'
+
+    #################################################################
+    ## WINDOW STUFF
+    ## We are defining and packing up some widgets.
+    ##
+
+    # Window formatting
+    root = tk.Tk()  # The window object
+    root.geometry("300x395")
+    root.resizable(False, True)
+    root.title("SMCM Exam Scheduler")
+    root['padx'] = 30
+    root['pady'] = 30
+    root.configure(bg='white')
+    root.iconbitmap(seahawk_icon_path)
+
+    # Logo formatting
+    logo = tk.PhotoImage(
+        file=logo_path)
+    logo = logo.subsample(2, 2)
+    logo_widget = tk.Label(root, image=logo, bg='white')
+
+    # Print icon formatting
+    print_icon = tk.PhotoImage(file=print_icon_path)
+    print_icon = print_icon.subsample(100, 100)
+
+    about_icon = tk.PhotoImage(file=about_icon_path)
+    about_icon = about_icon.subsample(18, 18)
+
+    help_icon = tk.PhotoImage(file=help_icon_path)
+    help_icon = help_icon.subsample(35, 35)
+
+    # Separator that goes under the title
+    sep = Separator(root, orient='horizontal')
+
+    # Define title
+    title_text = "Exam Scheduler"
+    title = tk.Message(root, text=title_text, width=400, anchor='center')
+    title.config(font=('calibri', 14), foreground=smcm_blue, bg='white')
+
+    # Define the labels for upload buttons
+    text_1_str = "Upload course schedule .csv file:"
+    text_1 = tk.Message(root, text=text_1_str, width=1000, bg='white')
+
+    text_2_str = "Upload finals schedule .csv file:"
+    text_2 = tk.Message(root, text=text_2_str, width=1000, bg='white')
+
+    # Upload buttons
+    upload_cschedule_button = tk.Button(root, text='Browse...', command=upload_callback)
+
+    upload_fschedule_button = tk.Button(root, text='Browse...', command=upload_callback2)
+
+    # Output buttons
+    save_output_button = tk.Button(root, text='Save Output...', command=save_output)
+
+    display_output_button = tk.Button(root, text='Display Output...', command=empty_button)
+
+    print_button = tk.Button(root, image=print_icon, width=25, height=25, command=empty_button)
+
+    about_button = tk.Button(root, image=about_icon, width=15, height=15, command=info_callback)
+
+    help_button = tk.Button(root, image=help_icon, width=15, height=15, command=help_callback)
+
+    ################################
+    # Put all the widgets into the window with a whole bunch of formatting
+    #
+
+    logo_widget.grid(row=0, column=0, columnspan=2)
+    title.grid(row=1, column=0, columnspan=2)
+    sep.grid(row=2, column=0, columnspan=2, sticky='ew')
+    text_1.grid(row=3, column=0, columnspan=2, padx=0, pady=(20, 5), sticky="W")
+    upload_cschedule_button.grid(row=4, column=0, columnspan=2, padx=20)
+    text_2.grid(row=5, column=0, columnspan=2, padx=0, pady=(10, 5), sticky='W')
+    upload_fschedule_button.grid(row=6, column=0, columnspan=2, padx=20)
+    save_output_button.grid(row=7, column=0, padx=12, pady=(20, 0), sticky='E')
+    display_output_button.grid(row=7, column=1, pady=(20, 0), sticky='W')
+    print_button.grid(row=8, column=0, columnspan=2, pady=(10, 0))
+    about_button.grid(row=9, column=1, columnspan=2, padx=(0, 12), sticky='E')
+    help_button.grid(row=9, column=2, columnspan=2, sticky='W')
+
+    # Make the window persistent
+    root.mainloop()
+
+
+GUI()
